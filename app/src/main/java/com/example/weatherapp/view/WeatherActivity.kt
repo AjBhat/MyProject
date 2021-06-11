300023f47f7c7dc3fcc4acf1a42c1c7aed6987f5
Ajith Ganesh <ajithsbhat005@gmail.com>
2021-06-11 21:18:40 +0530

    Forecast UI

--- app/src/main/java/com/example/weatherapp/view/WeatherActivity.kt
+++ app/src/main/java/com/example/weatherapp/view/WeatherActivity.kt
@@ -1,9 +1,10 @@
-package com.example.weatherapp
+package com.example.weatherapp.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.activity.viewModels
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import com.example.weatherapp.databinding.ActivityMainBinding
 import com.example.weatherapp.utils.ProgressDialog
@@ -22,26 +23,29 @@ class WeatherActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.hide()
         binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
+        progressDialog= ProgressDialog(this)
         setupObserver()
     }
 
     private fun setupObserver() {
         viewModel.weatherResp.observe(this, Observer {
-            when(it.status){
-                Status.SUCCESS ->{
+            when (it.status) {
+                Status.SUCCESS -> {
+                    progressDialog?.dismiss()
                     it.data?.let {
-                        textCity.text=it.name
-                        textTemp.text=it.main.temp.toString()+"°"
+                        textCity.text = it.name
+                        textTemp.text = it.main.temp.toString() + "°"
                     }
                 }
-                Status.LOADING ->{
-                    progressDialog= ProgressDialog(this)
+                Status.LOADING -> {
                     progressDialog?.show()
                 }
-                Status.ERROR ->{
-                    
+                Status.ERROR -> {
+
                 }
             }
         })
